b77893ccf2f5169dde29175cf3e22c77598ed416
ariibard <ariibardauill@gmail.com>
2023-09-24 21:47:30 -0300

    clase 3 proyecciones pob

--- clase 4/actividad clase 4/app.R
+++ clase 4/actividad clase 4/app.R
@@ -8,7 +8,7 @@ library(highcharter)
 
 #descargo el dataset ####
 
-options(timeout=1000000) # incrementamos el timeout debido a que la descarga es lenta
+#options(timeout=1000000) # incrementamos el timeout debido a que la descarga es lenta
 
 url = "http://datos.salud.gob.ar/dataset/2eff770c-1c2b-4a22-9281-c3b5e9412086/resource/c1253897-d507-41f7-a3e1-6ed756e7243b/download/tasa-mortalidad-infantil-deis-1990-2021.csv"
 
